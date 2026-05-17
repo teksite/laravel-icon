7bd3376e3726c2223e020a79a1dc678abdf2ed38
teksite <teksite.design@gmail.com>
2026-05-17 10:01:32 +0330

    update readme.md and config file

--- src/config/icon-setting.php
+++ src/config/icon-setting.php
@@ -2,23 +2,27 @@
 
 return [
 
-        /*
-        |--------------------------------------------------------------------------
-        | Cache Settings
-        |--------------------------------------------------------------------------
-        */
-        'cache' => [
-            'enabled' => env('SVG_ICONS_CACHE_ENABLED', false),
-            'ttl' => env('SVG_ICONS_CACHE_TTL', 86400), // 24 hours
-        ],
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Settings
+    |--------------------------------------------------------------------------
+    */
+    'cache'               => [
+        'enabled' => env('SVG_ICONS_CACHE_ENABLED', false),
+        'ttl'     => env('SVG_ICONS_CACHE_TTL', 86400), // 24 hours
+    ],
 
-        /*
-        |--------------------------------------------------------------------------
-        | Storage Path for Custom Icons
-        |--------------------------------------------------------------------------
-        */
-        'custom_solid_icon' => storage_path('app/svg-icons/solid.json'),
-        'custom_outline_icon' => storage_path('app/svg-icons/outline.json'),
-
-        'component' => 'components.icon',
+    /*
+    |--------------------------------------------------------------------------
+    | Storage Path for Custom Icons
+    |--------------------------------------------------------------------------
+    */
+    'custom_solid_icon'   => storage_path('app/svg-icons/solid.json'),
+    'custom_outline_icon' => storage_path('app/svg-icons/outline.json'),
+    /*
+    |--------------------------------------------------------------------------
+    | component to load icons
+    |--------------------------------------------------------------------------
+    */
+    'component'           => 'components.icon',
 ];
